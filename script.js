64727c4f7fc1fa7741e0bd0da4c4a76db4f61581
Stefan Kondratschin <kondratschin@gmail.com>
2024-06-08 15:35:23 +0200

    added moving logo

---
+++ script.js
@@ -0,0 +1,32 @@
+/**
+ * moves the logo to the upper corner
+ */
+function moveLogo() {
+    let logo = document.getElementById("splash-screen");
+    logo.classList.remove("logo-big");
+    logo.classList.add("logo-small");
+  }
+
+/**
+ * This needs to be moved to an event after loading database
+ */
+  setTimeout(moveLogo, 1000);
+
+
+  /**
+   * Hide an element by using ID
+   * 
+   * @param {string} id -  This is the ID of an element
+   */
+  function displayNone(id) {
+    document.getElementById(id).classList.add('d-none');
+}
+
+  /**
+   * show an element by using ID
+   * 
+   * @param {string} id -  This is the ID of an element
+   */
+function displayElement(id) {
+    document.getElementById(id).classList.remove('d-none');
+}
