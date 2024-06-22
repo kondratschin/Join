85edef3348be4f9b41770ccfe74db8085d709063
Stefan Kondratschin <kondratschin@gmail.com>
2024-06-22 15:01:52 +0200

    add task css fixes

--- addTask.js
+++ addTask.js
@@ -1,3 +1,4 @@
 function showContactDrp() {
+    document.getElementById('assign-field').classList.toggle('border-bottom-0');
     document.getElementById('contact-drp-dwn').classList.toggle('d-none');
 }
