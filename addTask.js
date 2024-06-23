240a152b477228eff8018ac257349b1296b9d368
Stefan Kondratschin <kondratschin@gmail.com>
2024-06-23 18:25:13 +0200

    add task css fixes

--- addTask.js
+++ addTask.js
@@ -6,6 +6,29 @@ function showContactDrp() {
 
 
 function prioritySelected(id, className, arrow) {
+    removeSelection(id);
     document.getElementById(id).classList.toggle(className);
     document.getElementById(id).classList.toggle(arrow);
+}
+
+
+function removeSelection(id) {
+    if (id == 'prio-baja') {
+        document.getElementById('prio-alta').classList.remove('prio-select-red');
+        document.getElementById('prio-alta').classList.remove('prio-select');
+        document.getElementById('prio-media').classList.remove('prio-select-orange');
+        document.getElementById('prio-media').classList.remove('prio-select');
+    };
+    if (id == 'prio-media') {
+        document.getElementById('prio-alta').classList.remove('prio-select-red');
+        document.getElementById('prio-alta').classList.remove('prio-select');
+        document.getElementById('prio-baja').classList.remove('prio-select-green');
+        document.getElementById('prio-baja').classList.remove('prio-select');
+    };
+    if (id == 'prio-alta') {
+        document.getElementById('prio-media').classList.remove('prio-select-orange');
+        document.getElementById('prio-media').classList.remove('prio-select');
+        document.getElementById('prio-baja').classList.remove('prio-select-green');
+        document.getElementById('prio-baja').classList.remove('prio-select');
+    };
 }
