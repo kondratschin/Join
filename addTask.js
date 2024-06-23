ec90b2ba04fdfdc30a43e9f7465f156cf94e1c44
Stefan Kondratschin <kondratschin@gmail.com>
2024-06-23 17:46:13 +0200

    add task css fixes

--- addTask.js
+++ addTask.js
@@ -3,3 +3,9 @@ function showContactDrp() {
     document.getElementById('contact-drp-dwn').classList.toggle('d-none');
     document.getElementById('arrow-drp-dwn').classList.toggle('flip-vertically');
 }
+
+
+function prioritySelected(id, className, arrow) {
+    document.getElementById(id).classList.toggle(className);
+    document.getElementById(id).classList.toggle(arrow);
+}
