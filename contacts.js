f544393ead366455662c753f0fac768ae304558e
Justin Koll <Justin.koll@icloud.com>
2024-06-24 20:07:01 +0200

    Finished delete function for Info box and Edit contact box

--- contacts.js
+++ contacts.js
@@ -67,9 +67,9 @@ function fadeOut(id) {
  * 
 */
 function clearInput() {
-    document.getElementById('addContactName').value = '';
-    document.getElementById('addContactEmail').value = '';
-    document.getElementById('addContactPhone').value = '';
+    document.getElementById('contactName').value = '';
+    document.getElementById('contactEmail').value = '';
+    document.getElementById('contactPhone').value = '';
 }
 
 
@@ -77,9 +77,9 @@ function addSubmitEvent() {
     document.getElementById('addContactInputForm').addEventListener('submit', async function (event) {
         event.preventDefault();
 
-        let contactName = document.getElementById('addContactName').value;
-        let contactEmail = document.getElementById('addContactEmail').value;
-        let contactPhone = document.getElementById('addContactPhone').value;
+        let contactName = document.getElementById('contactName').value;
+        let contactEmail = document.getElementById('contactEmail').value;
+        let contactPhone = document.getElementById('contactPhone').value;
 
         await createContact(contactName, contactEmail, contactPhone);
     });
@@ -90,9 +90,9 @@ function addEditSubmitEvent() {
     document.getElementById('editContactInputForm').addEventListener('submit', async function (event) {
         event.preventDefault();
 
-        let contactName = document.getElementById('editContactName').value;
-        let contactEmail = document.getElementById('editContactEmail').value;
-        let contactPhone = document.getElementById('editContactPhone').value;
+        let contactName = document.getElementById('contactName').value;
+        let contactEmail = document.getElementById('contactEmail').value;
+        let contactPhone = document.getElementById('contactPhone').value;
 
         await editContact(contactName, contactEmail, contactPhone);
     });
@@ -142,10 +142,12 @@ async function editContact(contactName, contactEmail, contactPhone) {
     } else {
         console.log("Error Editing contact.");
     };
+    getContacts();
 }
 
 
 function showAddContact(id) {
+    addNewContactCart();
     appear(id);
     fadeIn('addContactPopup');
     addSubmitEvent();
@@ -243,70 +245,23 @@ function editContact(color, initials, name, email, phone) {
 }
 
 
-function editCart(color, initials, name, email, phone) {
-    let popup = document.getElementById('addContactPopup');
-    showAddContact('addContactPopupBackground');
-    popup.innerHTML = `
-        <div class="addContactCardLeftSide">
-            <div class="addContactCardLeftSideContent">
-                <img src="./img/Capa-2.svg">
-                <h1>Edit contact</h1>
-                <div class="addContactCardLeftSideBreake">
+async function deleteContact(name) {
+    await deleteBackendContact(name);
+    await getContacts();
+    clearShowDetails();
+}
 
-                </div>
-            </div>
-        </div>
-        <div class="addContactCardRightSide">
-            <div class="addContactCardRightSideContent">
-                <div class="addContactCardRightSideContentImg">
-                    <p class="contactsEditContactInitials" style="background: ${color};">
-            ${initials}
-            </p>
-                </div>
-                <div class="addContactCardRightSideContentInputfieldsAndButtons">
 
-                    <div class="addContactCardRightSideContentInputfields">
-                        <div class="closeButtonDiv">
-                            <div class="closeButtonBackground"
-                                onclick="vanishAddConact()">
-                                <img class="addContactCloseButton" src="./img/close.svg">
-                            </div>
-                        </div>
-                         <form id="editContactInputForm">
-                            <div class="input">
-                                <input id="editContactName" class="addContactInput" placeholder="Name"
-                                    type="text" pattern="([A-Z][a-z]{1,}\s[A-Z][a-z]{1,})+"
-                                    title="Please enter at least two words with initial capital letters"
-                                    >
-                                <img src="./img/person.svg">
-                            </div>
-                            <div class="input">
-                                <input id="editContactEmail" class="addContactInput" placeholder="Email"
-                                    type="email" >
-                                <img src="./img/mail.svg">
-                            </div>
-                             <div class="input">
-                                <input id="editContactPhone" class="addContactInput" placeholder="Phone"
-                                    type="tel" pattern="([0-9])+" title="Only Nummbers allowed" >
-                                <img src="./img/call.svg">
-                            </div>
-                            <div class="addContactButtons">
-                                <button type="reset" class="cancelButton"
-                                     onclick="vanishAddConact()">Delete
-                                </button>
-                                 <button type="submit" class="editContactButton">Save <img src="./img/buttonCheck.svg">
-                                 </button>
-                            </div>
-                        </form>
+async function deleteBackendContact(name) {
+    let response = await fetch(BASE_URL + "/contacts" + "/" + accName + "/" + name + ".json", {
+        method: "DELETE",
+    });
+    return responseAsJason = await response.json();
+}
 
-                    </div>
 
-                </div>
-            </div>
-        </div>
-    `;
-    document.getElementById('editContactName').value = name;
-    document.getElementById('editContactEmail').value = email;
-    document.getElementById('editContactPhone').value = phone;
-    addEditSubmitEvent();
+function clearShowDetails() {
+    let detailBox = document.getElementById('contactsRightsideContentInfoBox');
+    detailBox.innerHTML = '';
+    fadeOut('contactsRightsideContentInfoBox');
 }
