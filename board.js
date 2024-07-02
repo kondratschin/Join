ee569c116a0280387d7f9f1ea786892a8e45f932
Stefan Kondratschin <kondratschin@gmail.com>
2024-07-02 21:30:00 +0200

    implemented tasks into board

--- board.js
+++ board.js
@@ -14,7 +14,7 @@ function getName() {
 
 function load() {
     addPlus();
-    getTasks();  // Aufrufen der getTasks beim Laden der Seite
+    getTasks().then(renderToDoList);
 }
 
 
@@ -41,9 +41,6 @@ async function getTasks() {
     } catch (error) {
         console.error("Error fetching tasks:", error);
     }
-
-    renderTasks(); // Diese Funktion müsste implementiert werden, um die Tasks anzuzeigen
-    renderOverlayTask();
 }
 
 
@@ -52,6 +49,7 @@ async function getTasks() {
  * @returns 
  */
 function renderOverlayTask(taskCategory = 'toDo') { // Add parameter to choose the task category
+    displayElement('task-overlay');
     let content = document.getElementById('task-overlay');
     const tasksInCategory = tasks[taskCategory]; // Get tasks from the specified category
 
@@ -110,6 +108,69 @@ function renderOverlayTask(taskCategory = 'toDo') { // Add parameter to choose t
     `;
 }
 
+function renderToDoList() {
+    renderList('toDo');
+}
+
+
+function renderList(taskCategory) { // Add parameter to choose the task category
+    let content = document.getElementById('toDoList');
+    let tasksInCategory = tasks[taskCategory]; // Get tasks from the specified category
+
+    if (tasksInCategory.length === 0) {
+        content.innerHTML = "<p>No tasks available in this category.</p>";
+        return;
+    }
+
+    let task = tasksInCategory[0]; // Display the first task for this example
+    let selectedContact = task.selectedContacts.length > 0 ? task.selectedContacts[0] : { color: '#ccc', initials: '', name: 'Unknown' };
+        // Access the first element of the chosenCategory array
+        let chosenCategory = task.chosenCategory[0];
+
+    content.innerHTML = /*html*/ `
+    <div class="task-overlay-head">
+    <span class="task-overlay-category ${chosenCategory === 'Technical Task' ? 'technical-task' : 'user-story-task'}">${chosenCategory}</span> 
+        <div class="closeButtonBackground">
+        <img onclick="displayNone('task-overlay')" src="./img/close.svg" alt="">
+        </div>
+    </div>
+    </div>
+    <span class="task-overlay-title">${task.id}</span>
+    <span class="task-overlay-text">${task.taskDescription}</span>
+    <table class="task-overlay-text">
+        <tr>
+            <td>Due date:</td>
+            <td>${task.taskDate}</td>
+        </tr>
+        <tr>
+            <td>Priority:</td>
+            <td>${task.priority}</td>
+        </tr>
+    </table>
+    <span class="task-overlay-text">Assigned to:</span>
+    <div class="task-overlay-assigned">
+        <div class="task-overlay-ass-person">
+            <div class="initialsContact-small" style="background: ${selectedContact.color}">
+                ${selectedContact.initials}
+            </div>
+            <span class="pddng-lft-12">${selectedContact.name}</span>
+        </div>
+    </div>
+    <span class="task-overlay-text">Subtasks</span>
+    <li> ${task.subTaskList}</li>
+    <div class="task-overlay-foot">
+        <div class="overlay-action highlight-gray">
+        <img id="recycle-small-img" class="plus" src="./img/recycle.svg" alt="">
+        <span>Delete</span>
+        </div>
+        <img src="./img/separator-small.svg" class="sep-small" alt="">
+        <div class="overlay-action highlight-gray">
+        <img id="edit-small-img" class="plus" src="./img/edit-small.svg" alt="">
+        <span>Edit</span>
+        </div>
+    </div>
+    `;
+}
 
 
 
