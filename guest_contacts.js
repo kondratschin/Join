d3b712a86a08cf3e8c5d0c58397f2d4d16d449c2
Stefan Kondratschin <kondratschin@gmail.com>
2024-08-06 23:33:09 +0200

    added local version of contacts.js

--- guest_contacts.js
+++ guest_contacts.js
@@ -1,131 +1,95 @@
-const BASE_URL = "https://join-fda66-default-rtdb.europe-west1.firebasedatabase.app/";
 let accName = getName();
 let contacts = [];
 let alphabetContainer = [];
 
-
 window.addEventListener("resize", checkWidthAndAddClickListener);
 
+document.addEventListener("DOMContentLoaded", function() {
+    if (!localStorage.getItem('contacts')) {
+        loadJSONData();
+    } else {
+        getContacts();
+    }
+});
 
-/**
- * function exists in board.js and must be removed
- * @returns 
- */
 function getName() {
     let name = localStorage.getItem('userName');
-    return name; // Return the retrieved name
+    return name;
 }
 
-
-/**
- * Hide an element by using ID
- * 
- * 
- * @param {string} id -  This is the ID of an element
-*/
 function displayNone(id) {
     document.getElementById(id).classList.add('d-none');
 }
 
-
-/**
- * Show an element by using ID
- * 
- * 
- * @param {string} id -  This is the ID of an element
-*/
 function displayElement(id) {
     document.getElementById(id).classList.remove('d-none');
 }
 
-
-/**
- * Stop the propagation of the displayNone event
- * 
- * 
- * @param {Event} event - The click event
-*/
 function stopPropagation(event) {
     event.stopPropagation();
 }
-
-
 
 function appear(id) {
     document.getElementById(id).classList.remove('vanish');
     document.getElementById(id).classList.add('appear');
 }
 
-
 function vanish(id) {
     document.getElementById(id).classList.remove('appear');
     document.getElementById(id).classList.add('vanish');
 }
-
 
 function fadeIn(id) {
     document.getElementById(id).classList.remove('fadeOut');
     document.getElementById(id).classList.add('fadeIn');
 }
 
-
 function fadeOut(id) {
     document.getElementById(id).classList.remove('fadeIn');
     document.getElementById(id).classList.add('fadeOut');
 }
-
 
 function mobileFadeIn(id) {
     document.getElementById(id).classList.remove('mobileOptionFadeOut');
     document.getElementById(id).classList.add('mobileOptionFadeIn');
 }
 
-
 function mobileFadeOut(id) {
     document.getElementById(id).classList.remove('mobileOptionFadeIn');
     document.getElementById(id).classList.add('mobileOptionFadeOut');
 }
 
-
-/**
- * Clears the inputfield in the add contact cart
- * 
- * 
-*/
 function clearInput() {
     document.getElementById('contactName').value = '';
     document.getElementById('contactEmail').value = '';
     document.getElementById('contactPhone').value = '';
 }
 
-
 function addSubmitEvent() {
-    document.getElementById('addContactInputForm').addEventListener('submit', async function (event) {
+    document.getElementById('addContactInputForm').addEventListener('submit', function (event) {
         event.preventDefault();
 
         let contactName = document.getElementById('contactName').value;
         let contactEmail = document.getElementById('contactEmail').value;
         let contactPhone = document.getElementById('contactPhone').value;
 
-        await createContact(contactName, contactEmail, contactPhone);
+        createContact(contactName, contactEmail, contactPhone);
     });
 }
 
-
-async function addEditSubmitEvent(color, oldName) {
-    document.getElementById('editContactInputForm').addEventListener('submit', async function (event) {
+function addEditSubmitEvent(color, oldName) {
+    document.getElementById('editContactInputForm').addEventListener('submit', function (event) {
         event.preventDefault();
-      
+
         let contactName = document.getElementById('contactName').value;
         let contactEmail = document.getElementById('contactEmail').value;
         let contactPhone = document.getElementById('contactPhone').value;
 
-        await editContact(contactName, contactEmail, contactPhone, color, oldName);
+        editContact(contactName, contactEmail, contactPhone, color, oldName);
     });
 }
 
-
-async function createContact(contactName, contactEmail, contactPhone) {
+function createContact(contactName, contactEmail, contactPhone) {
     let hue = Math.random() * 360;
     let randomColor = `hsl(${hue}, 70%, 50%)`
     let newContact = {
@@ -134,25 +98,14 @@ async function createContact(contactName, contactEmail, contactPhone) {
         phone: contactPhone,
         color: randomColor
     };
-    let response = await fetch(BASE_URL + "contacts/" + accName + "/" + contactName + ".json", {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newContact)
-    });
-    if (response.ok) {
-    
-    } else {
-        console.log("Error creating contact.");
-    };
-    showSuccesPopup()
+    contacts[contactName] = newContact;
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    showSuccesPopup();
     getContacts();
     vanishAddConact();
 }
 
-
-async function editContact(contactName, contactEmail, contactPhone, contactColor, oldName) {
+function editContact(contactName, contactEmail, contactPhone, contactColor, oldName) {
     let initials = contactName.match(/\b(\w)/g).join('');
     let editData = {
         name: contactName,
@@ -160,24 +113,13 @@ async function editContact(contactName, contactEmail, contactPhone, contactColor
         phone: contactPhone,
         color: contactColor
     };
-    let response = await fetch(BASE_URL + "contacts/" + accName + "/" + contactName + ".json", {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(editData)
-    });
-    if (response.ok) {
-        alert("Edit Contact successfully.");
-    } else {
-        console.log("Error Editing contact.");
-    };
-    await deleteBackendContact(oldName);
+    delete contacts[oldName];
+    contacts[contactName] = editData;
+    localStorage.setItem('contacts', JSON.stringify(contacts));
     getContacts();
     vanishAddConact();
     showContactDetails(contactColor, initials, contactName, contactEmail, contactPhone);
 }
-
 
 function showSuccesPopup() {
     fadeIn('contactSuccesBox');
@@ -185,17 +127,14 @@ function showSuccesPopup() {
     setTimeout(fadeSuccesPopup, 2000);
 }
 
-
 function fadeSuccesPopup() {
     fadeOut('contactSuccesBox');
     setTimeout(dnoneSuccesPopup, 100);
 }
 
-
 function dnoneSuccesPopup() {
     displayNone('contactSuccesBox');
 }
-
 
 function showAddContact(id) {
     addNewContactCart();
@@ -204,7 +143,6 @@ function showAddContact(id) {
     addSubmitEvent();
 }
 
-
 function showEditContact(id, color, initials, name, email, phone) {
     editCart(color, initials, name, email, phone);
     appear(id);
@@ -212,14 +150,11 @@ function showEditContact(id, color, initials, name, email, phone) {
     addEditSubmitEvent(color, name);
 }
 
-
 function vanishAddConact() {
     vanish('addContactPopupBackground');
     clearInput();
     fadeOut('addContactPopup');
 }
-
-
 
 function createContactSortArray() {
     alphabetContainer = [];
@@ -227,25 +162,20 @@ function createContactSortArray() {
     const endCharCode = 'Z'.charCodeAt(0);
 
     for (let i = startCharCode; i <= endCharCode; i++) {
-        alphabetContainer.push(
-            {
-                letter: String.fromCharCode(i),
-                list: []
-            }
-        );
+        alphabetContainer.push({
+            letter: String.fromCharCode(i),
+            list: []
+        });
     }
 }
 
-
-async function getContacts() {
-    let response = await fetch(BASE_URL + "contacts/" + accName + ".json");
-    let responseAsJson = await response.json();
-    let contactsAsArray = Object.keys(responseAsJson);
-    sortContactlist(responseAsJson, contactsAsArray);
+function getContacts() {
+    contacts = JSON.parse(localStorage.getItem('contacts')) || {};
+    let contactsAsArray = Object.keys(contacts);
+    sortContactlist(contacts, contactsAsArray);
     showContactsInList();
     checkWidthAndAddClickListener();
 }
-
 
 function sortContactlist(responseAsJson, contactsAsArray) {
     createContactSortArray();
@@ -257,20 +187,16 @@ function sortContactlist(responseAsJson, contactsAsArray) {
     };
 }
 
-
 function pushContactIntoSort(responseAsJson, contactsAsArray, alphabetChar) {
     if (responseAsJson[contactsAsArray]['name'].charAt(0) == alphabetChar['letter']) {
-        alphabetChar['list'].push(
-            {
-                name: responseAsJson[contactsAsArray]['name'],
-                email: responseAsJson[contactsAsArray]['email'],
-                phone: responseAsJson[contactsAsArray]['phone'],
-                color: responseAsJson[contactsAsArray]['color'],
-            }
-        )
+        alphabetChar['list'].push({
+            name: responseAsJson[contactsAsArray]['name'],
+            email: responseAsJson[contactsAsArray]['email'],
+            phone: responseAsJson[contactsAsArray]['phone'],
+            color: responseAsJson[contactsAsArray]['color'],
+        });
     }
 }
-
 
 function showContactsInList() {
     let contactCarts = document.getElementById('contactCarts');
@@ -282,14 +208,12 @@ function showContactsInList() {
     }
 }
 
-
 function createContactListAlphabethicContainer(sortLetterNr) {
     if (sortLetterNr['list'].length > 0) {
         printContactAlphabethicContainer(sortLetterNr);
         showContactInList(sortLetterNr);
     }
 }
-
 
 function showContactInList(sortLetterNr) {
     for (let y = 0; y < sortLetterNr['list'].length; y++) {
@@ -298,21 +222,12 @@ function showContactInList(sortLetterNr) {
     }
 }
 
-
-async function deleteContact(name) {
-    await deleteBackendContact(name);
-    await getContacts();
+function deleteContact(name) {
+    delete contacts[name];
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    getContacts();
     clearShowDetails();
 }
-
-
-async function deleteBackendContact(name) {
-    let response = await fetch(BASE_URL + "/contacts" + "/" + accName + "/" + name + ".json", {
-        method: "DELETE",
-    });
-    return responseAsJason = await response.json();
-}
-
 
 function clearShowDetails() {
     let detailBox = document.getElementById('contactsRightsideContentInfoBox');
@@ -320,19 +235,16 @@ function clearShowDetails() {
     fadeOut('contactsRightsideContentInfoBox');
 }
 
-
 function showContactDetailsMobile() {
     document.getElementById('contactsRightsideContent').classList.remove('d-none');
     document.getElementById('contactsContent').classList.add('d-none');
     addEventlistenerMobileMenu();
 }
 
-
 function backToContactList() {
     document.getElementById('contactsRightsideContent').classList.add('d-none');
     document.getElementById('contactsContent').classList.remove('d-none');
 }
-
 
 if (window.location.pathname === '/contacts.html') {
     function checkWidthAndAddClickListener() {
@@ -354,27 +266,37 @@ if (window.location.pathname === '/contacts.html') {
     }
 }
 
-
 function showMobileOptions(id) {
     appear(id);
     mobileFadeIn(id);
 }
 
 function addEventlistenerMobileMenu() {
-document.addEventListener('click', function (event) {
-    let excludedObjects = document.querySelectorAll('.excludedObjectMobile');
-    let clickedElement = event.target;
-    let isExcluded = false;
+    document.addEventListener('click', function (event) {
+        let excludedObjects = document.querySelectorAll('.excludedObjectMobile');
+        let clickedElement = event.target;
+        let isExcluded = false;
 
-    excludedObjects.forEach(function (object) {
-      if (object.contains(clickedElement)) {
-        isExcluded = true;
-      }
+        excludedObjects.forEach(function (object) {
+            if (object.contains(clickedElement)) {
+                isExcluded = true;
+            }
+        });
+
+        if (!isExcluded) {
+            mobileFadeOut('dropdownContainerContent');
+            vanish('dropdownContainerContent');
+        }
     });
-  
-    if (!isExcluded) {
-        mobileFadeOut('dropdownContainerContent');
-        vanish('dropdownContainerContent');
-    }
-  });
+}
+
+function loadJSONData() {
+    fetch('guest.json')
+        .then(response => response.json())
+        .then(data => {
+            localStorage.setItem('contacts', JSON.stringify(data.contacts.Guest));
+            localStorage.setItem('tasks', JSON.stringify(data.tasks.Guest));
+            getContacts();
+        })
+        .catch(error => console.error('Error loading JSON data:', error));
 }
