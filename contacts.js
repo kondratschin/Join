8144e43b37fe80c8894b23cd3c8dd20cdaa34506
Kondratschin <Stefan.Kondratschin@spray.com>
2024-08-08 10:33:10 +0200

    removed unneccessary files

--- contacts.js
+++ contacts.js
@@ -488,3 +488,17 @@ function createContactLocally(contactName, contactEmail, contactPhone) {
     getContactsLocally();
     vanishAddConact();
 }
+
+
+/**
+ * Check if guest is logged in and check local storage for contacts, if not available load guest.json data
+ */
+document.addEventListener("DOMContentLoaded", function() {
+    if (accName && accName.trim() !== '') {
+        if (!localStorage.getItem('contacts')) {
+            loadJSONDataContacts();
+        } else {
+            getContactsLocally();
+        }
+    }
+});
